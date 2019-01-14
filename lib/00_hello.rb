787a3324975ccdceb9aa4918a332b61e58f5849c
KuzniClem <clement@kuzniewycz.fr>
2019-01-14 10:33:19 +0100

    .

---
+++ lib/00_hello.rb
@@ -0,0 +1,16 @@
+def hello
+	puts "A qui ai-je l'honneur?"
+	print "> "
+	puts "Hello#{greet(gets.chomp)}!"
+end
+
+def greet(name)
+	case name
+	when ""
+		""
+	else
+		", #{name}"
+	end
+end
+
+hello
