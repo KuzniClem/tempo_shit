2168befb5e1cc462689069cad9333ee21127ce79
KuzniClem <clement@kuzniewycz.fr>
2019-01-14 14:50:16 +0100

    pig_latin implement

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -4,13 +4,13 @@ def who_is_bigger(a, b, c)
 end
 
 def reverse_upcase_noLTA(str)
-	str.reverse.upcase.gsub(/(L|T|A)/, '')
+	str.reverse.upcase.gsub(/[LTA]/, '')
 end
 
 def array_42(arr)
 	arr.include?(42)
 end
 
-def magic_array(arr)
-	arr.flatten.uniq.map { |e| (e % 3 == 0)? nil : e *= 2 }.compact.sort
+def magic_array(ar)
+	ar.flatten.uniq.map{|e|(e%3==0)?nil:e*=2}.compact.sort
 end
